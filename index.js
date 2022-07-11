c0ec9cb862f364de9133b8908dac252af25f3284
Romain TAILLANDIER <romain.taillandier@wassa.io>
2022-07-11 21:56:51 +0200

    start to setup project

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
+require('dotenv').config();
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.NODE_PORT || 5000
+
+console.log( process.env.NODE_PORT)
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
