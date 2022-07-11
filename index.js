1912ac3673abfd513dc180c3520fdb5f14fb7a36
Romain TAILLANDIER <romain.taillandier@wassa.io>
2022-07-11 22:01:18 +0200

    add cool

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 require('dotenv').config();
+const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.NODE_PORT || 5000
@@ -11,4 +12,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
